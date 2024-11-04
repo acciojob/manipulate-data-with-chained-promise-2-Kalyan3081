10bb0a1c8df0a35071fac2cba88429eb0a3e005e
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-11-04 13:14:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getNumbers() {
   // Returns a promise that resolves with an array of numbers
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       resolve([1, 2, 3, 4]);
     }, 3000);
@@ -10,7 +10,7 @@ function getNumbers() {
 getNumbers()
   .then((numbers) => {
     // First promise: filter out odd numbers after 1 second
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         const evenNumbers = numbers.filter((num) => num % 2 === 0);
         document.getElementById("output").textContent = evenNumbers.join(", ");
@@ -20,11 +20,10 @@ getNumbers()
   })
   .then((evenNumbers) => {
     // Second promise: multiply even numbers by 2 after 2 seconds
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         const multipliedNumbers = evenNumbers.map((num) => num * 2);
-        document.getElementById("output").textContent =
-          multipliedNumbers.join(", ");
+        document.getElementById("output").textContent = multipliedNumbers.join(", ");
         resolve(multipliedNumbers);
       }, 2000); // Delay of 2 seconds
     });
